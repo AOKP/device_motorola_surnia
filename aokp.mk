3e75ca0b8a6cea23ce09985b537cdb97df7b9e28
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2017-01-05 22:36:22 +0100

    AOKP-fy
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -18,10 +18,11 @@ $(call inherit-product, device/motorola/surnia/full_surnia.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
+TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := aokp_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
